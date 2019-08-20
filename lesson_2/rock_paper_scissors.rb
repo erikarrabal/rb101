5aab7194d00f8f2b37d544021850a8dddad71e00
Erik Arrabal <erikarrabal@gmail.com>
2019-08-20 09:44:05 +0000

    Modified Rock Paper Scissors game

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,43 +1,46 @@
 VALID_CHOICES = ["rock", "paper", "scissors"]
 
+def test_method
+  prompt("test message")
+end
+
 def prompt(message)
   puts "=> #{message}"
 end
 
 def display_results(player, computer)
   if (player == "rock" && computer == "scissors") ||
-      (player == "paper" && computer == "rock") ||
-      (player == "scissors" && computer == "paper")
-    prompt("You won!")
+     (player == "paper" && computer == "rock") ||
+     (player == "scissors" && computer == "paper")
+    return "You won!"
   elsif (player == "rock" && computer == "paper") ||
-      (player == "paper" && computer == "scissors") ||
-      (player == "scissors" && computer == "rock")
-    prompt("You lost!")
+        (player == "paper" && computer == "scissors") ||
+        (player == "scissors" && computer == "rock")
+    return "You lost!"
   else
-    prompt("It's a tie!")
+    return "It's a tie!"
   end
 end
-
 
 loop do
   choice = ""
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(", ")}")
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = gets.chomp
-  
+
     if VALID_CHOICES.include?(choice)
       break
     else
       prompt("That's not a valid choice.")
     end
   end
-  
+
   computer_choice = VALID_CHOICES.sample
-  
+
   puts "You chose: #{choice}; The computer chose: #{computer_choice}."
-  
-  display_results(choice, computer_choice)
-  
+
+  prompt (display_results(choice, computer_choice))
+
   prompt("Do you want to play again?")
   answer = gets.chomp
   break unless answer.downcase.start_with?("y")
